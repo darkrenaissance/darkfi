816e46ceffa7765f761868a48dc367db93d4945a
rachel-rose <ihateface@autistici.org>
2020-10-20 20:43:37 +0200

    removed redudant line

--- run_jubjub2.sh
+++ run_jubjub2.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -x
 racket lisp/jj.rkt || exit $?
 python scripts/compile.py --supervisor jj.psm --output jubjub.zcd || exit $?
-cargo run --release --bin jubjub
-cargo run --bin zkvm -- init jubjub.zcd jubjub.zts
-cargo run --bin zkvm -- prove jubjub.zcd jubjub.zts proofs/jubjub.params jubjub.prf
-cargo run --bin zkvm -- verify jubjub.zcd jubjub.zts jubjub.prf
-cargo run --bin zkvm -- show jubjub.prf
+cargo run --release --bin zkvm -- init jubjub.zcd jubjub.zts
+cargo run --release --bin zkvm -- prove jubjub.zcd jubjub.zts proofs/jubjub.params jubjub.prf
+cargo run --release --bin zkvm -- verify jubjub.zcd jubjub.zts jubjub.prf
+cargo run --release --bin zkvm -- show jubjub.prf
