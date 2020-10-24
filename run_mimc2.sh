f51ddea3fd4b7f5238a0e65248ea9d133d62cd9e
rachel-rose <ihateface@autistici.org>
2020-10-24 12:48:32 +0200

    bash script for mimc on zkvm

--- run_mimc2.sh
+++ run_mimc2.sh
@@ -3,5 +3,5 @@ python scripts/preprocess.py proofs/mimc.psm > /tmp/mimc.psm || exit $?
 python scripts/compile.py --supervisor /tmp/mimc.psm --output mimc.zcd || exit $?
 cargo run --release --bin zkvm -- init mimc.zcd mimc.zts
 cargo run --release --bin zkvm -- prove mimc.zcd mimc.zts proofs/mimc.params mimc.prf
-#cargo run --release --bin zkvm -- verify mimc.zcd mimc.zts mimc.prf
-#cargo run --release --bin zkvm -- show mimc.prf
+cargo run --release --bin zkvm -- verify mimc.zcd mimc.zts mimc.prf
+cargo run --release --bin zkvm -- show mimc.prf
