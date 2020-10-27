a4c863e4bbafe0f0ff09a2ecee8b93d5aed5f95c
ada <a@a.a>
2020-10-27 20:45:34 +0100

    fn* for zk circuit

--- lisp/new.lisp
+++ lisp/new.lisp
@@ -1,18 +1,18 @@
-(def! circuit (zk* [x] (
+(def! circuit (fn* [x] (
     (def! bits (unpack-bits x 256))
-    (map (fn* [b] (println 
+    (map (fn* [b] (println b
                     '(add lc0 one) 
                     '(sub b)
                     '(add lc1 x)
                     'enforce)
               ) bits)
-    (map (fn* [b] (println '(add lc0 b) 
-                           'double-coeff-lc)
-              ) bits)
-    (println 'reset-coeff-lc
-    '(sub lc0 x)
-    '(add lc1 one)
-    'enforce)
+   (map (fn* [b] (println b '(add lc0 b) 
+                          'double-coeff-lc)
+             ) bits)
+   (println 'reset-coeff-lc
+   '(sub lc0 x)
+   '(add lc1 one)
+   'enforce)
 )))
 (def! dec "73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000000")
 (circuit dec)
