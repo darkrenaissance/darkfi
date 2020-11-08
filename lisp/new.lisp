96d391900f02ae6df95082b6487dace33798e4f3
ada <a@a.a>
2020-11-08 20:10:31 +0100

    added zkcircuit values on eval

--- lisp/new.lisp
+++ lisp/new.lisp
@@ -2,20 +2,16 @@
 (def! one "0000000000000000000000000000000000000000000000000000000000000001")
 (def! bits (unpack-bits x))
 (defzk! circuit ())
-
 (def! cvalues (map (fn* [b] (eval
                     (add lc0 one) 
-                        )) bits))
-
-;;(map (fn* [b] (zkcons! circuit (
-;;                    (add lc0 one) 
-;;                    (sub lc0 b)
-;;                    (add lc1 x))
-;;        )) bits)
-;;                    'reset-coeff-lc
-;;                    (sub lc0 x)
-;;                    (add lc1 one)
-;;                    'enforce)
-;;            ))
+                    (sub lc0 b)
+                    (add lc1 x)
+                    enforce)
+                        ) bits))
+(def! cs (concat cvalues (list 
+                 'reset-coeff-lc
+                 (sub lc0 x)
+                 (add lc1 one)
+                 'enforce)))
 (println "bit-dec")
-(zkcons! circuit cvalues)
+(zkcons! circuit cs)
