93f48047248f37a2b932b386493b0d12745b32a9
narodnik <x@x.org>
2021-02-23 16:02:54 +0100

    Document run_network.sh script

--- run_network.sh
+++ run_network.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# Run this script then
+#   python scripts/monitor-p2p.py
+# to view the network topology
+
 declare -a arr=(
+    # Seed node
     "cargo run --bin dfi -- -r 8999 --accept 127.0.0.1:9999 -D --log /tmp/darkfi/seed.log"
+    # Server with no outgoing connections
     "cargo run --bin dfi -- -r 9000 --accept 127.0.0.1:10001 --seeds 127.0.0.1:9999 --log /tmp/darkfi/server.log"
+    # Client with 1 outgoing connection
     "cargo run --bin dfi -- -r 9002 --seeds 127.0.0.1:9999 --slots 1 --log /tmp/darkfi/client.log"
+    # Client with 1 outgoing connection
     "cargo run --bin dfi -- -r 9003 --seeds 127.0.0.1:9999 --slots 1 --log /tmp/darkfi/client1.log"
+    # Client with 1 outgoing connection
     "cargo run --bin dfi -- -r 9004 --seeds 127.0.0.1:9999 --slots 1 --log /tmp/darkfi/client2.log"
-    "cargo run --bin dfi -- -r 9005 --accept 127.0.0.1:10002 --seeds 127.0.0.1:9999 --log /tmp/darkfi/server1.log"
+    # Server/client with 2 outgoing connections
+    "cargo run --bin dfi -- -r 9005 --accept 127.0.0.1:10002 --seeds 127.0.0.1:9999 --log /tmp/darkfi/server1.log" --slots 2
 )
 
 for cmd in "${arr[@]}"; do {
