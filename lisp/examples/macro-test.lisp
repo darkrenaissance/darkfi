10916184a2ac3beb4d1b4992142d518a70e2e717
plato <plato@x.com>
2021-03-05 11:15:21 +0100

    fix zk-* alloc-input

--- lisp/examples/macro-test.lisp
+++ lisp/examples/macro-test.lisp
@@ -4,7 +4,7 @@
         (let* [v1 (gensym)
                v2 (gensym)] (
         `(alloc ~v1 ~var)
-        `(def! output (alloc ~v2 (square ~var)))
+        `(def! output (alloc-input ~v2 (square ~var)))
         `(enforce  
             (scalar::one ~v1) 
             (scalar::one ~v1) 
@@ -21,7 +21,7 @@
                var (gensym)] (
         `(alloc ~v1 ~val1)
         `(alloc ~v2 ~val2)
-        `(def! result (alloc ~var (* ~val1 ~val2)))
+        `(def! result (alloc-input ~var (* ~val1 ~val2)))
         `(enforce  
             (scalar::one ~v1) 
             (scalar::one ~v2) 
@@ -36,11 +36,13 @@
         (let* [u2 (gensym)
                v2 (gensym)
                u2v2 (gensym)
-               EDWARDS_D (gensym)] (
+               EDWARDS_D (gensym)
+               result (gensym)] (
         `(def! ~EDWARDS_D (alloc-const ~EDWARDS_D (scalar "2a9318e74bfa2b48f5fd9207e6bd7fd4292d7f6d37579d2601065fd6d6343eb1")))
         `(def! ~u2 (alloc ~u2 (get (nth (nth (zk-square ~val1) 0) 3) "v2")))
         `(def! ~v2 (alloc ~v2 (get (nth (nth (zk-square ~val2) 0) 3) "v2")))
         `(def! result (alloc-input ~u2v2 (get (last (last (zk-mul ~u2 ~v2))) "result")))        
+        ;; `(def! result (alloc-input ~result (* ~u2 ~v2)))        
         `(enforce  
             ((scalar::one::neg ~u2) (scalar::one ~v2))
             (scalar::one cs::one)
@@ -57,9 +59,8 @@
 (def! param-v (scalar "6d81d3a9cb45dedbe6fb2a6e1e22ab50ad46f1b0473b803b3caefab9380b6a8b"))
 (prove 
   (
-    ;; (def! result1 (zk-square param1))
-    ;; (println 'result1_map (get (nth (nth result1 0) 3) "v2"))
-    ;; (println 'result_mul (get (last (last (zk-mul param1 param1))) "result"))
+    ;; (println (zk-square param1))
+    ;; (println (zk-mul param1 param2))
     (println (zk-witness param-u param-v))
   )
 )
