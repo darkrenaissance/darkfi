11dea54b442efe92952f44f38b28011663d37011
rachel-rose <ihateface@autistici.org>
2021-05-28 20:12:55 +0200

    fixed typo on wallet schema

--- res/schema.sql
+++ res/schema.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS keys(
 );
 CREATE INDEX IF NOT EXISTS key_public on keys(key_public);
 CREATE TABLE IF NOT EXISTS coins(
-    coin BLOB NOT NULL
-    witness BLOB NOT NULL
+    coin BLOB NOT NULL,
+    witness BLOB NOT NULL,
     serial BLOB NOT NULL,
     value INT NOT NULL,
     coin_blind BLOB NOT NULL,
