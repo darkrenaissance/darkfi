8978255cd76b29a9aeb413e5092f2ebc4ee28561
rachel-rose <ihateface@autistici.org>
2021-06-21 11:56:46 +0200

    added more columns to 'coins' table

--- res/schema.sql
+++ res/schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS keys(
     key_public BLOB NOT NULL,
     key_private BLOB NOT NULL
 );
+PRAGMA foreign_keys=on;
 CREATE TABLE IF NOT EXISTS coins(
     coin_id INTEGER PRIMARY KEY NOT NULL,
     coin BLOB NOT NULL,
@@ -11,7 +12,10 @@ CREATE TABLE IF NOT EXISTS coins(
     serial BLOB NOT NULL,
     value INT NOT NULL,
     coin_blind BLOB NOT NULL,
-    valcom_blind BLOB NOT NULL
+    valcom_blind BLOB NOT NULL,
+    key_id INTEGER NOT NULL,
+    FOREIGN KEY (key_id)
+        REFERENCES keys (key_id)
 );
 CREATE TABLE IF NOT EXISTS cashier(
     key_id INTEGER PRIMARY KEY NOT NULL,
