ec302e0d53cb12ee15c47894a77cf5a6fe366fdd
lunar-mining <ihateface@autistici.org>
2021-07-22 09:48:56 +0200

    renamed bitcoin_bridge.rs to cashier.rs

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,7 +1,7 @@
 pub mod gateway;
 pub mod reqrep;
-pub mod bitcoin_bridge;
+pub mod cashier;
 
 pub use gateway::{GatewayClient, GatewayService, GatewaySlabsSubscriber};
 
-pub use bitcoin_bridge::{BitcoinKeys, CashierService, CashierClient};
+pub use cashier::{BitcoinKeys, CashierService, CashierClient};
