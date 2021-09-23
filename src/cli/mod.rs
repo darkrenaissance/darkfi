e3ebdf820a4e2fd789cfd1051c3ed19c7cdca406
parazyd <parazyd@dyne.org>
2021-09-23 15:35:35 +0200

    cli: Remove old code.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,13 +1,7 @@
 pub mod cashierd_cli;
 pub mod cli_config;
-pub mod darkfid_cli;
-pub mod drk_cli;
 pub mod gatewayd_cli;
 
 pub use cashierd_cli::CashierdCli;
 pub use cli_config::{CashierdConfig, Config, DarkfidConfig, DrkConfig, GatewaydConfig};
-pub use darkfid_cli::DarkfidCli;
-pub use drk_cli::Asset;
-pub use drk_cli::DrkCli;
-pub use drk_cli::{TransferParams, WithdrawParams};
 pub use gatewayd_cli::GatewaydCli;
