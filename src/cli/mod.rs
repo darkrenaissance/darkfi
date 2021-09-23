110c7d67702d5a6fe0dd8f0110ebd769f01c872a
parazyd <parazyd@dyne.org>
2021-09-23 18:11:50 +0200

    cli: Remove old cashierd_cli code.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,7 +1,5 @@
-pub mod cashierd_cli;
 pub mod cli_config;
 pub mod gatewayd_cli;
 
-pub use cashierd_cli::CashierdCli;
 pub use cli_config::{CashierdConfig, Config, DarkfidConfig, DrkConfig, GatewaydConfig};
 pub use gatewayd_cli::GatewaydCli;
