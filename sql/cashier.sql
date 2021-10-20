cb773a61e65f0bd3b030c90f65b010bcf58e4d75
ghassmo <ghxxm@protonmail.com>
2021-10-20 13:06:01 +0300

    clean up schema

--- sql/cashier.sql
+++ sql/cashier.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS main_keypairs(
-    keypair_id INTEGER PRIMARY KEY NOT NULL,
-   	token_key_private BLOB NOT NULL,
-    token_key_public BLOB NOT NULL,
+	keypair_id INTEGER PRIMARY KEY NOT NULL,
+	token_key_private BLOB NOT NULL,
+	token_key_public BLOB NOT NULL,
 	network BLOB NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS deposit_keypairs(
-    keypair_id INTEGER PRIMARY KEY NOT NULL,
-    d_key_public BLOB NOT NULL,
-   	token_key_private BLOB NOT NULL,
-    token_key_public BLOB NOT NULL,
+	keypair_id INTEGER PRIMARY KEY NOT NULL,
+	d_key_public BLOB NOT NULL,
+	token_key_private BLOB NOT NULL,
+	token_key_public BLOB NOT NULL,
 	network BLOB NOT NULL,
 	token_id BLOB NOT NULL,
 	mint_address BLOB NOT NULL,
@@ -17,10 +17,10 @@ CREATE TABLE IF NOT EXISTS deposit_keypairs(
 );
 
 CREATE TABLE IF NOT EXISTS withdraw_keypairs(
-    keypair_id INTEGER PRIMARY KEY NOT NULL,
-    token_key_public BLOB NOT NULL,
+	keypair_id INTEGER PRIMARY KEY NOT NULL,
+	token_key_public BLOB NOT NULL,
 	d_key_private BLOB NOT NULL,
-    d_key_public BLOB NOT NULL,
+	d_key_public BLOB NOT NULL,
 	network BLOB NOT NULL,
 	token_id BLOB NOT NULL,
 	mint_address BLOB NOT NULL,
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS keys(
-    key_id INTEGER PRIMARY KEY NOT NULL,
-    key_public BLOB NOT NULL,
-    key_private BLOB NOT NULL
+	key_id INTEGER PRIMARY KEY NOT NULL,
+	key_public BLOB NOT NULL,
+	key_private BLOB NOT NULL
 );
 PRAGMA foreign_keys=on;
 CREATE TABLE IF NOT EXISTS coins(
-    coin_id INTEGER PRIMARY KEY NOT NULL,
-    coin BLOB NOT NULL,
-    serial BLOB NOT NULL,
-    coin_blind BLOB NOT NULL,
-    valcom_blind BLOB NOT NULL,
-    value INT NOT NULL,
-    token_id INT NOT NULL,
-    witness BLOB NOT NULL,
-   	secret BLOB NOT NULL,
+	coin_id INTEGER PRIMARY KEY NOT NULL,
+	coin BLOB NOT NULL,
+	serial BLOB NOT NULL,
+	coin_blind BLOB NOT NULL,
+	valcom_blind BLOB NOT NULL,
+	value INT NOT NULL,
+	token_id INT NOT NULL,
+	witness BLOB NOT NULL,
+	secret BLOB NOT NULL,
 	is_spent BLOB NOT NULL
 );
