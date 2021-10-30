18d0ad37d3d39c233f20da0c2219ec9da930419f
narodnik <s>
2021-10-30 15:09:55 +0200

    working vm2 in example/halo2/

--- example/halo2/doit.sh
+++ example/halo2/doit.sh
@@ -4,6 +4,6 @@ python script/zkas.py proof/mint.zk --bincode
 du -sh proof/mint.zk.bin
 python script/zkas.py proof/mint.zk
 #python script/zkas.py proof/mint.zk
-cd example/vm/
+cd example/halo2/
 cargo run --release --bin vm2
 
