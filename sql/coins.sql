650db5f15a261c653d90d8c8ce5e04ab73b3fdcb
parazyd <parazyd@dyne.org>
2021-12-01 21:22:46 +0100

    sql: Separate schemas into multiple files.

--- sql/coins.sql
+++ sql/coins.sql
@@ -1,17 +1,10 @@
-CREATE TABLE IF NOT EXISTS keys(
-	key_id INTEGER PRIMARY KEY NOT NULL,
-	key_public BLOB NOT NULL,
-	key_private BLOB NOT NULL
-);
-PRAGMA foreign_keys=on;
 CREATE TABLE IF NOT EXISTS coins(
 	coin BLOB PRIMARY KEY NOT NULL,
 	serial BLOB NOT NULL,
 	coin_blind BLOB NOT NULL,
 	valcom_blind BLOB NOT NULL,
-	value INTEGER NOT NULL,
+	value BLOB NOT NULL,
 	token_id BLOB NOT NULL,
-	witness BLOB NOT NULL,
 	secret BLOB NOT NULL,
 	is_spent BOOLEAN NOT NULL,
 	nullifier BLOB NOT NULL
