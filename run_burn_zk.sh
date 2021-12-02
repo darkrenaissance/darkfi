dce2a3294cab34f2887f3180bcc62684f916baae
parazyd <parazyd@dyne.org>
2021-12-02 19:41:57 +0100

    bin: Rename vm2 to vm.

--- run_burn_zk.sh
+++ run_burn_zk.sh
@@ -3,5 +3,5 @@ python script/zkas.py proof/burn.zk --bincode
 du -sh proof/burn.zk.bin
 python script/zkas.py proof/burn.zk
 #python script/zkas.py proof/mint.zk
-cargo run --release --bin vm2_burn
+cargo run --release --bin vm_burn
 
--- run_mint_zk.sh
+++ run_mint_zk.sh
@@ -3,5 +3,4 @@ python script/zkas.py proof/mint.zk --bincode
 du -sh proof/mint.zk.bin
 python script/zkas.py proof/mint.zk
 #python script/zkas.py proof/mint.zk
-cargo run --release --bin vm2
-
+cargo run --release --bin vm
