7905599b7bf7986902461467b8d962639f43785e
lunar-mining <ihateface@autistici.org>
2021-12-20 14:08:29 +0100

    util/ mod: added loader

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,10 +1,12 @@
 pub mod address;
+pub mod loader;
 pub mod net_name;
 pub mod parse;
 pub mod path;
 pub mod token_list;
 
 pub use address::Address;
+pub use loader::ContractLoader;
 pub use net_name::NetworkName;
 pub use parse::{assign_id, decode_base10, encode_base10, generate_id, generate_id2};
 pub use path::{expand_path, join_config_path};
