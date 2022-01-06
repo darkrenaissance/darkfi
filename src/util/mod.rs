0f2a9b3275a9bec002c749121e45209ac85c00bc
Dastan-glitch <Dastan.ManDan@gmail.com>
2022-01-06 15:53:14 -0500

    Minor formatting

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,11 +6,11 @@ pub mod parse;
 pub mod path;
 pub mod token_list;
 
+pub use async_util::sleep;
 pub use loader::ContractLoader;
 pub use net_name::NetworkName;
 pub use parse::{assign_id, decode_base10, encode_base10, generate_id, generate_id2};
 pub use path::{expand_path, join_config_path};
-pub use async_util::sleep;
 pub use token_list::{DrkTokenList, TokenList};
 
 pub use address::Address;
