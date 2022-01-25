8da37aaa5e03fabafc509ed0e37b2ac4948ef0ae
parazyd <parazyd@dyne.org>
2022-01-25 23:43:51 +0100

    Remove old python zkas scripts.

--- script/run_burn_zk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-python zkas.py ../proof/burn.zk --bincode
-du -sh ../proof/burn.zk.bin
-python zkas.py ../proof/burn.zk
-cargo run --all-features --release --example vm_burn
--- script/run_mint_zk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-python zkas.py ../proof/mint.zk --bincode
-du -sh ../proof/mint.zk.bin
-python zkas.py ../proof/mint.zk
-cargo run --all-features --release --example vm
