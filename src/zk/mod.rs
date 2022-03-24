2de7656f8c5e285d998e7b63c5850a053267c4a5
parazyd <parazyd@dyne.org>
2022-03-24 18:59:37 +0100

    zk: Implement GreaterThan chip used for scalar comparison.

--- src/zk/mod.rs
+++ src/zk/mod.rs
@@ -1,5 +1,9 @@
 /// Halo2 arithmetic chip
 pub mod arith_chip;
+/// Even bits lookup table
+pub mod even_bits;
+/// Greather than gadget
+pub mod greater_than;
 
 /// Halo2 zkas virtual machine
 pub mod vm;
