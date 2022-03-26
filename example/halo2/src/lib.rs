143449db4eb5fe410deef2b48b05f1f90da107fe
parazyd <parazyd@dyne.org>
2022-03-26 09:49:27 +0100

    example/halo2: Remove old files.

--- example/halo2/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod constants;
-pub mod crypto;
-pub mod endian;
-pub mod error;
-pub mod proof;
-pub mod serial;
-pub mod spec;
--- example/halo2/src/spec.rs
+++
@@ -1,6 +0,0 @@
-use crate::constants::util::gen_const_array;
-
-pub fn i2lebsp<const NUM_BITS: usize>(int: u64) -> [bool; NUM_BITS] {
-    assert!(NUM_BITS <= 64);
-    gen_const_array(|mask: usize| (int & (1 << mask)) != 0)
-}
