4a4b40d7be4a9820f9955eee4278fc774e360879
parazyd <parazyd@dyne.org>
2022-04-03 15:24:56 +0200

    blockchain: Implement initial blockstore API.

--- src/blockchain2/mod.rs
+++ src/blockchain2/mod.rs
@@ -1,4 +1,29 @@
+use std::io;
+
+use crate::{
+    impl_vec,
+    util::serial::{Decodable, Encodable, ReadExt, VarInt, WriteExt},
+    Result,
+};
+
 pub mod blockstore;
 pub mod nfstore;
 pub mod rootstore;
 pub mod txstore;
+
+impl Encodable for blake3::Hash {
+    fn encode<S: io::Write>(&self, mut s: S) -> Result<usize> {
+        s.write_slice(self.as_bytes())?;
+        Ok(32)
+    }
+}
+
+impl Decodable for blake3::Hash {
+    fn decode<D: io::Read>(mut d: D) -> Result<Self> {
+        let mut bytes = [0u8; 32];
+        d.read_slice(&mut bytes)?;
+        Ok(bytes.into())
+    }
+}
+
+impl_vec!(blake3::Hash);
