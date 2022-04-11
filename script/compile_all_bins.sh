e85a29a05c87d09c06a3469dc11640d18625c938
ghassmo <ghxxm@protonmail.com>
2022-04-11 10:01:00 +0400

    fix ci check "compile all bins"

--- script/compile_all_bins.sh
+++ script/compile_all_bins.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-bins="$(find bin -maxdepth 1 | tail -n+2 | sed 's,bin/,,' | tr '\n' ' ')"
-make BINS="$bins"
+bins="$(find bin -maxdepth 1 | tail -n+2 | sed 's,bin/,,' | tr '\n' ' ' |  sed 's,tau,,')"
+bins_tau="$(find bin/tau -maxdepth 1 | sed 's,bin/tau/,,' | grep -w 'taud\|tau-cli' | tr '\n' ' ')"
+make BINS="$bins_tau $bins"
