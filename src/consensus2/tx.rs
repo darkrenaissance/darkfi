e24c3b00b4dbf4d097a27c80e9a66ce7f641ec76
parazyd <parazyd@dyne.org>
2022-04-16 20:40:35 +0200

    consensus: Use actual transactions.

--- src/consensus2/tx.rs
+++ src/consensus2/tx.rs
@@ -2,15 +2,14 @@ use std::io;
 
 use crate::{
     impl_vec, net,
+    tx::Transaction,
     util::serial::{Decodable, Encodable, SerialDecodable, SerialEncodable, VarInt},
     Result,
 };
 
 /// Temporary transaction representation.
 #[derive(Debug, Clone, PartialEq, SerialEncodable, SerialDecodable)]
-pub struct Tx {
-    pub payload: String,
-}
+pub struct Tx(pub Transaction);
 
 impl net::Message for Tx {
     fn name() -> &'static str {
