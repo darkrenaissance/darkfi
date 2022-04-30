28129fe33acc2c9039120825daad41a5bf2cf4d3
parazyd <parazyd@dyne.org>
2022-05-01 00:40:40 +0200

    zk: Fix module.

--- src/zk/circuit/mod.rs
+++ src/zk/circuit/mod.rs
@@ -1,14 +1,8 @@
 pub mod burn_contract;
 pub use burn_contract::BurnContract;
 
-
-pub mod spend_contract;
-pub mod lead_contract;
-
-
-
 pub mod mint_contract;
 pub use mint_contract::MintContract;
 
-pub use spend_contract::SpendContract;
+pub mod lead_contract;
 pub use lead_contract::LeadContract;
