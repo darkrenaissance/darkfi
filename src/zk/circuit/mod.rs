57d4e2443a89ac55b18466d667b5edf6fc76b72b
parazyd <parazyd@dyne.org>
2022-05-01 10:48:19 +0200

    zk/circuit: Disable lead contract.

--- src/zk/circuit/mod.rs
+++ src/zk/circuit/mod.rs
@@ -4,5 +4,5 @@ pub use burn_contract::BurnContract;
 pub mod mint_contract;
 pub use mint_contract::MintContract;
 
-pub mod lead_contract;
-pub use lead_contract::LeadContract;
+// pub mod lead_contract;
+// pub use lead_contract::LeadContract;
