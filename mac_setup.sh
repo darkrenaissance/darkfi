cf17c7647e020b0da9b03809aad040b99e191ed1
hades921 <99681487+SubGlitch1@users.noreply.github.com>
2022-05-23 21:59:08 +0100

    Create mac_setup.sh

---
+++ mac_setup.sh
@@ -0,0 +1,8 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install cmake
+brew install gcc
+brew install jq
+brew install pkgconf
+brew install llvm@13
+brew install freetype
+brew install expat
