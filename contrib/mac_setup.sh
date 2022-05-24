de2cca62937e7a8d387edd5d04350b087952b673
hades921 <99681487+SubGlitch1@users.noreply.github.com>
2022-05-24 09:21:34 +0300

    setyp.sh

---
+++ contrib/mac_setup.sh
@@ -0,0 +1,20 @@
+if ! command -v brew &> /dev/null
+then
+   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install cmake
+    brew install gcc
+    brew install jq
+    brew install pkgconf
+    brew install llvm@13
+    brew install freetype
+    brew install expat
+fi
+
+
+brew install cmake
+brew install gcc
+brew install jq
+brew install pkgconf
+brew install llvm@13
+brew install freetype
+brew install expat
