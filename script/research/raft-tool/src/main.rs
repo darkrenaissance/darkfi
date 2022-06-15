385d4477570f5082d5a08cbb1ec1f11ab63d243f
ghassmo <ghxxm@protonmail.com>
2022-06-15 17:03:12 +0300

    research/raft-tool: decrypt tasks in tau datastore using secret key

--- script/research/raft-tool/src/main.rs
+++ script/research/raft-tool/src/main.rs
@@ -1,5 +1,7 @@
 use std::{fs::File, io::Write};
 
+use crypto_box::{aead::Aead, Box, SecretKey, KEY_SIZE};
+
 use darkfi::{
     raft::DataStore,
     util::{
@@ -8,6 +10,13 @@ use darkfi::{
     },
     Result,
 };
+
+mod error;
+mod month_tasks;
+mod task_info;
+mod util;
+
+use crate::{task_info::TaskInfo, util::load};
 
 #[derive(Debug)]
 pub struct Info<T> {
@@ -32,6 +41,19 @@ struct EncryptedTask {
     payload: Vec<u8>,
 }
 
+fn decrypt_task(encrypt_task: &EncryptedTask, secret_key: &SecretKey) -> Option<TaskInfo> {
+    let public_key = secret_key.public_key();
+    let msg_box = Box::new(&public_key, secret_key);
+
+    let nonce = encrypt_task.nonce.as_slice();
+    let decrypted_task = match msg_box.decrypt(nonce.into(), &encrypt_task.payload[..]) {
+        Ok(m) => m,
+        Err(_) => return None,
+    };
+
+    deserialize(&decrypted_task).ok()
+}
+
 type PrivmsgId = u32;
 
 #[derive(Debug, SerialEncodable, SerialDecodable)]
@@ -45,6 +67,24 @@ struct Privmsg {
 fn extract_taud() -> Result<String> {
     let db_path = expand_path(&"~/.config/darkfi/tau/tau.db").unwrap();
     let datastore = DataStore::<EncryptedTask>::new(&db_path.to_str().unwrap())?;
+
+    let sk_path = expand_path(&"~/.config/darkfi/tau/secret_key").unwrap();
+
+    let sk = {
+        let loaded_key = load::<String>(&sk_path);
+
+        if loaded_key.is_err() {
+            log::error!(
+                "Could not load secret key from file, \
+                  please run \"taud --help\" for more information"
+            );
+            return Ok("Load secret_key error".into())
+        }
+
+        let sk_bytes = hex::decode(loaded_key.unwrap())?;
+        let sk_bytes: [u8; KEY_SIZE] = sk_bytes.as_slice().try_into()?;
+        SecretKey::try_from(sk_bytes)?
+    };
 
     println!("Extracting db from: {:?}", db_path);
 
@@ -60,14 +100,20 @@ fn extract_taud() -> Result<String> {
     // Logs
     let mut logs = vec![];
     for log in sled_logs {
-        logs.push(Log { term: log.term, msg: deserialize(&log.msg)? });
+        let encrypt_task: EncryptedTask = deserialize(&log.msg)?;
+        let task_info = decrypt_task(&encrypt_task, &sk).unwrap();
+        logs.push(Log { term: log.term, msg: task_info });
     }
 
     // Commits
     let mut commits = vec![];
     for commit in &sled_commits {
-        commits
-            .push(EncryptedTask { nonce: commit.nonce.clone(), payload: commit.payload.clone() });
+        let task_info = decrypt_task(
+            &EncryptedTask { nonce: commit.nonce.clone(), payload: commit.payload.clone() },
+            &sk,
+        )
+        .unwrap();
+        commits.push(task_info);
     }
 
     // Voted for
@@ -85,7 +131,7 @@ fn extract_taud() -> Result<String> {
         terms.push(term);
     }
 
-    let info = Info::<EncryptedTask> { logs, commits, voted_for, terms };
+    let info = Info::<TaskInfo> { logs, commits, voted_for, terms };
     let info_string = format!("{:#?}", info);
     Ok(info_string)
 }
