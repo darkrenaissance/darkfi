bed7986a5381f34425b591954f4b8093583fcfa1
aggstam <aggelosstam13@gmail.com>
2022-08-19 21:49:54 +0300

    consensu: seeds to be ignored during syncing, localnet: lilith node added

--- contrib/localnet/tmux_sessions.sh
+++ contrib/localnet/tmux_sessions.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 set -e
 
-# Start a tmux session with two consensus nodes and a non-consensus node, and
+# Start a tmux session with a lilith node, two consensus nodes, a non-consensus node, and
 # a faucet that's able to mint tokens.
 
 tmux new-session -d
-tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -v -c darkfid0.toml" Enter
+tmux send-keys "../../lilith -c lilith_config.toml" Enter
 sleep 2
 tmux split-window -v
-tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -v -c darkfid1.toml" Enter
-sleep 2
-tmux split-window -h
-tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -v -c darkfid2.toml" Enter
+tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -c darkfid0.toml" Enter
 sleep 2
 tmux select-pane -t 0
 tmux split-window -h
-tmux send-keys "LOG_TARGETS='!sled,!net' ../../faucetd -v -c faucetd.toml" Enter
+tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -c darkfid1.toml" Enter
+sleep 2
+tmux select-pane -t 1
+tmux split-window -h
+tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -c darkfid2.toml" Enter
+sleep 2
+tmux select-pane -t 3
+tmux split-window -h
+tmux send-keys "LOG_TARGETS='!sled,!net' ../../faucetd -c faucetd.toml" Enter
 tmux attach
