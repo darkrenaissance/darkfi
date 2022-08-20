22512a7552424f15a4f45e13eac62f19dc39b52f
aggstam <aggelosstam13@gmail.com>
2022-08-20 17:44:11 +0300

    contrib/localnet/tmux_sessions: log target for lilith added

--- contrib/localnet/tmux_sessions.sh
+++ contrib/localnet/tmux_sessions.sh
@@ -5,20 +5,20 @@ set -e
 # a faucet that's able to mint tokens.
 
 tmux new-session -d
-tmux send-keys "../../lilith -c lilith_config.toml" Enter
+tmux send-keys "LOG_TARGETS='!MessageSubsystem::notify' ../../lilith -v -c lilith_config.toml" Enter
 sleep 2
 tmux split-window -v
-tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -c darkfid0.toml" Enter
+tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -v -c darkfid0.toml" Enter
 sleep 2
 tmux select-pane -t 0
 tmux split-window -h
-tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -c darkfid1.toml" Enter
+tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -v -c darkfid1.toml" Enter
 sleep 2
 tmux select-pane -t 1
 tmux split-window -h
-tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -c darkfid2.toml" Enter
+tmux send-keys "LOG_TARGETS='!sled' ../../darkfid -v -c darkfid2.toml" Enter
 sleep 2
 tmux select-pane -t 3
 tmux split-window -h
-tmux send-keys "LOG_TARGETS='!sled,!net' ../../faucetd -c faucetd.toml" Enter
+tmux send-keys "LOG_TARGETS='!sled,!net' ../../faucetd -v -c faucetd.toml" Enter
 tmux attach
