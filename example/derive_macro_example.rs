22af89a66dc3b5a6cc9349802a11669b481b9482
Dastan-glitch <Dastan.ManDan@gmail.com>
2022-09-19 19:41:42 +0300

    add simple test example

---
+++ example/derive_macro_example.rs
@@ -0,0 +1,12 @@
+use darkfi::util::serial::SerialEncodable;
+
+#[derive(Debug, SerialEncodable)]
+struct Test {
+    one: u64,
+    two: u64,
+}
+
+fn main() {
+    let test = Test { one: 1, two: 2 };
+    println!("Test: {:?}", test);
+}
