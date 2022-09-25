6703111d60c16824adf99f3f52f8b8e74422d281
ghassmo <ghxxm@protonmail.com>
2022-09-25 13:36:00 +0400

    serial/derive: show error when use for enums

--- src/serial/derive/src/lib.rs
+++ src/serial/derive/src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro2::Span;
 use proc_macro_crate::{crate_name, FoundCrate};
 use syn::{Ident, Item, ItemStruct};
 
-use darkfi_derive_internal::{enum_ser, struct_de, struct_ser};
+use darkfi_derive_internal::{struct_de, struct_ser};
 
 #[proc_macro_derive(SerialEncodable, attributes(skip_serialize))]
 pub fn darkfi_serialize(input: TokenStream) -> TokenStream {
@@ -19,7 +19,6 @@ pub fn darkfi_serialize(input: TokenStream) -> TokenStream {
 
     let res = match syn::parse(input).unwrap() {
         Item::Struct(strc) => struct_ser(&strc, cratename),
-        Item::Enum(enu) => enum_ser(&enu, cratename),
         _ => todo!("Not implemented type"),
     };
 
