ad29ec69ff7c9bec3d91134579fcbcff8951cf6c
Dastan-glitch <Dastan.ManDan@gmail.com>
2022-10-20 03:43:05 +0300

    bin/dao: update run_demo script to use already built binary

--- bin/dao/dao-cli/run_demo.sh
+++ bin/dao/dao-cli/run_demo.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
-cargo run create 110 110 1 2
-addr=$(cargo run addr | cut -d " " -f 4)
+dao="../../../dao"
+
+$dao create 110 110 1 2
+addr=$($dao addr | cut -d " " -f 4)
 addr2=$(echo $addr | cut -c 2-)
 addr3=${addr2::-1}
-echo $addr3
+echo "address: $addr3"
 
-cargo run mint 1000000 $addr3
+$dao mint 1000000 $addr3
 
-alice=$(cargo run keygen)
-alice=$(cargo run keygen | cut -d " " -f 4)
+alice=$($dao keygen)
+alice=$($dao keygen | cut -d " " -f 4)
 alice2=$(echo $alice | cut -c 2-)
 alice3=${alice2::-1}
-echo $alice3
+echo "alice key: $alice3"
 
-bob=$(cargo run keygen)
-bob=$(cargo run keygen | cut -d " " -f 4)
+bob=$($dao keygen)
+bob=$($dao keygen | cut -d " " -f 4)
 bob2=$(echo $bob | cut -c 2-)
 bob3=${bob2::-1}
-echo $bob3
+echo "bob key: $bob3"
 
-charlie=$(cargo run keygen)
-charlie=$(cargo run keygen | cut -d " " -f 4)
+charlie=$($dao keygen)
+charlie=$($dao keygen | cut -d " " -f 4)
 charlie2=$(echo $charlie | cut -c 2-)
 charlie3=${charlie2::-1}
-echo $charlie3
+echo "charlie key: $charlie3"
 
-cargo run airdrop $alice3 10000
-cargo run airdrop $bob3 100000
-cargo run airdrop $charlie3 10000
+$dao airdrop $alice3 10000
+$dao airdrop $bob3 100000
+$dao airdrop $charlie3 10000
 
-proposal=$(cargo run propose $alice3 $charlie3 10000 | cut -d " " -f 3)
+proposal=$($dao propose $alice3 $charlie3 10000 | cut -d " " -f 3)
 proposal2=$(echo $proposal | cut -c 2-)
-proposal3=${proposal2::-1}
-echo $proposal3
+proposal3=${proposal2::-5}
+echo "Proposal bulla: $proposal3"
 
-cargo run vote $alice3 yes
-cargo run vote $bob3 yes
-cargo run vote $charlie3 no
+$dao vote $alice3 yes
+$dao vote $bob3 yes
+$dao vote $charlie3 no
 
-cargo run exec $proposal3
+$dao exec $proposal3
