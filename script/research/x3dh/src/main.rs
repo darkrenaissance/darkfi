8ae84ad7f9615e8b352fc7b46944e3b71ee49f93
parazyd <parazyd@dyne.org>
2022-10-31 18:16:13 +0100

    Add license header to source files.

--- script/research/x3dh/src/main.rs
+++ script/research/x3dh/src/main.rs
@@ -1,3 +1,21 @@
+/* This file is part of DarkFi (https://dark.fi)
+ *
+ * Copyright (C) 2020-2022 Dyne.org foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 //! https://signal.org/docs/specifications/x3dh/x3dh.pdf
 //! https://signal.org/docs/specifications/doubleratchet/doubleratchet.pdf
 use std::collections::{HashMap, VecDeque};
--- src/sdk/src/crypto/constants/fixed_bases/value_commit_r.rs
+++ src/sdk/src/crypto/constants/fixed_bases/value_commit_r.rs
@@ -1,3 +1,21 @@
+/* This file is part of DarkFi (https://dark.fi)
+ *
+ * Copyright (C) 2020-2022 Dyne.org foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use pasta_curves::{arithmetic::CurveAffine, group::ff::PrimeField, pallas};
 
 /// The value commitment is used to check balance between inputs and outputs. The value is
