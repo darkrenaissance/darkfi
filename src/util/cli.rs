b6a0170863217f8c92d26de77e4138c7b22fa79e
parazyd <parazyd@dyne.org>
2022-12-12 17:25:26 +0100

    util/cli: Aesthetics

--- src/util/cli.rs
+++ src/util/cli.rs
@@ -65,7 +65,7 @@ pub fn spawn_config(path: &Path, contents: &[u8]) -> Result<()> {
 
         let mut file = fs::File::create(path)?;
         file.write_all(contents)?;
-        println!("Config file created in '{:?}'. Please review it and try again.", path);
+        println!("Config file created in {:?}. Please review it and try again.", path);
         std::process::exit(2);
     }
 
