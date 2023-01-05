02f022f49147d2c687e680e9ce816d5322ecd46a
mohab metwally <mohab-metwally@riseup.net>
2023-01-06 01:28:04 +0200

    [contrib] clean log files

--- contrib/localnet/darkfid-five-nodes/clean.sh
+++ contrib/localnet/darkfid-five-nodes/clean.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 rm -rf darkfid0 darkfid1 darkfid2 darkfid3 darkfid4
+# clean log files
+rm /tmp/f_history.log  /tmp/lead_history.log &>/dev/null &
+touch /tmp/f_history.log
+touch /tmp/lead_history.log
+
+
