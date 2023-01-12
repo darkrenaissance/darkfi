5044141b24537b28b018016089a6cae561d8f4cd
aggstam <aggelosstam13@gmail.com>
2023-01-12 13:57:51 +0200

    fmt

--- example/zk.rs
+++ example/zk.rs
@@ -31,10 +31,7 @@ use darkfi_sdk::{
     crypto::pedersen::pedersen_commitment_u64,
     pasta::{
         arithmetic::CurveAffine,
-        group::{
-            ff::Field,
-            Curve,
-        },
+        group::{ff::Field, Curve},
         pallas,
     },
 };
