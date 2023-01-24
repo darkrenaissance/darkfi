cb484e6307ab5b39b4f9f743181ba559358c57d1
parazyd <parazyd@dyne.org>
2023-01-24 17:22:21 +0100

    localnet/single-node: Add hacky way to update genesis.

--- contrib/localnet/darkfid-single-node/tmux_sessions.sh
+++ contrib/localnet/darkfid-single-node/tmux_sessions.sh
@@ -6,8 +6,22 @@ set -e
 
 if [ "$1" = "-v" ]; then
 	verbose="-v"
+	shift
 else
 	verbose=""
+fi
+
+if [ "$1" = "now" ]; then
+	now="$(date +%s)"
+
+	sed \
+		-e 's/pub const SLOT_TIME: .*/pub const SLOT_TIME: u64 = 20;/' \
+		-e 's/pub const FINAL_SYNC_DUR: .*/pub const FINAL_SYNC_DUR: u64 = 15;/' \
+		-e "s/pub static ref TESTNET_GENESIS_TIMESTAMP: .*/pub static ref TESTNET_GENESIS_TIMESTAMP: Timestamp = Timestamp($now);/" \
+		-e "s/pub static ref TESTNET_BOOTSTRAP_TIMESTAMP: .*/pub static ref TESTNET_BOOTSTRAP_TIMESTAMP: Timestamp = Timestamp($now);/" \
+		-i ../../../src/consensus/constants.rs
+
+	exit
 fi
 
 tmux new-session -d
