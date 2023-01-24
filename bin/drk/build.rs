a56047ffc18bf030849ee8c17d871ea5a0cedd72
parazyd <parazyd@dyne.org>
2023-01-24 18:30:24 +0100

    drk: Only Linux gets the fun.

--- bin/drk/build.rs
+++ bin/drk/build.rs
@@ -19,6 +19,7 @@
 use pkg_config::probe_library;
 
 fn main() {
+    #[cfg(target_os = "linux")]
     if probe_library("libout123").is_ok() && probe_library("libmpg123").is_ok() {
         println!("cargo:rustc-cfg=feature=\"play\"");
     }
