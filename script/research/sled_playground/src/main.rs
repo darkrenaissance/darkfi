daca750bda92f0e97a094acd5cb4154878e342c4
aggstam <aggelosstam13@gmail.com>
2023-03-09 16:19:56 +0200

    script/research/sled_playground: overlay2 added, wrapping everything in there

--- script/research/sled_playground/src/main.rs
+++ script/research/sled_playground/src/main.rs
@@ -21,6 +21,9 @@ use sled::{transaction::ConflictableTransactionError, Config, Transactional};
 pub mod overlay;
 use overlay::SledOverlay;
 
+pub mod overlay2;
+use overlay2::SledOverlay2;
+
 const TREE_1: &str = "_tree1";
 const TREE_2: &str = "_tree2";
 
@@ -77,6 +80,54 @@ fn main() -> Result<(), sled::Error> {
         })
         .unwrap();
     db.flush()?;
+
+    // Verify sled contains keys
+    assert_eq!(tree_1.get(b"key_a")?, Some(b"val_a".into()));
+    assert_eq!(tree_1.get(b"key_b")?, Some(b"val_b".into()));
+    assert_eq!(tree_1.get(b"key_c")?, Some(b"val_c".into()));
+
+    assert_eq!(tree_2.get(b"key_d")?, Some(b"val_d".into()));
+    assert_eq!(tree_2.get(b"key_e")?, Some(b"val_e".into()));
+    assert_eq!(tree_2.get(b"key_f")?, Some(b"val_f".into()));
+
+    // Testing overlay2
+    // Initialize database overlay
+    let config = Config::new().temporary(true);
+    let db = config.open()?;
+    let mut overlay = SledOverlay2::new(&db, &[&TREE_1, &TREE_2])?;
+    // We keep seperate trees for validation
+    let tree_1 = db.open_tree(TREE_1)?;
+    let tree_2 = db.open_tree(TREE_2)?;
+
+    // Insert some values to the overlays
+    overlay.insert(TREE_1, b"key_a", b"val_a")?;
+    overlay.insert(TREE_1, b"key_b", b"val_b")?;
+    overlay.insert(TREE_1, b"key_c", b"val_c")?;
+
+    overlay.insert(TREE_2, b"key_d", b"val_d")?;
+    overlay.insert(TREE_2, b"key_e", b"val_e")?;
+    overlay.insert(TREE_2, b"key_f", b"val_f")?;
+
+    // Verify they are in the overlay
+    assert_eq!(overlay.get(TREE_1, b"key_a")?, Some(b"val_a".into()));
+    assert_eq!(overlay.get(TREE_1, b"key_b")?, Some(b"val_b".into()));
+    assert_eq!(overlay.get(TREE_1, b"key_c")?, Some(b"val_c".into()));
+
+    assert_eq!(overlay.get(TREE_2, b"key_d")?, Some(b"val_d".into()));
+    assert_eq!(overlay.get(TREE_2, b"key_e")?, Some(b"val_e".into()));
+    assert_eq!(overlay.get(TREE_2, b"key_f")?, Some(b"val_f".into()));
+
+    // Verify they are not in sled
+    assert_eq!(tree_1.get(b"key_a")?, None);
+    assert_eq!(tree_1.get(b"key_b")?, None);
+    assert_eq!(tree_1.get(b"key_c")?, None);
+
+    assert_eq!(tree_2.get(b"key_d")?, None);
+    assert_eq!(tree_2.get(b"key_e")?, None);
+    assert_eq!(tree_2.get(b"key_f")?, None);
+
+    // Now execute all tree baches in the overlay
+    assert_eq!(overlay.execute(), Ok(()));
 
     // Verify sled contains keys
     assert_eq!(tree_1.get(b"key_a")?, Some(b"val_a".into()));
