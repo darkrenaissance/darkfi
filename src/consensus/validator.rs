1633dc542e4215f6155e5d6f55e1217807aae4f6
aggstam <aggelosstam13@gmail.com>
2023-05-01 20:19:30 +0300

    src/consensus/validator: removed unused import

--- src/consensus/validator.rs
+++ src/consensus/validator.rs
@@ -45,7 +45,7 @@ use super::{
 use crate::{
     blockchain::{Blockchain, BlockchainOverlay, BlockchainOverlayPtr},
     rpc::jsonrpc::JsonNotification,
-    runtime::vm_runtime::{Runtime, SMART_CONTRACT_ZKAS_DB_NAME},
+    runtime::vm_runtime::Runtime,
     system::{Subscriber, SubscriberPtr},
     tx::Transaction,
     util::time::Timestamp,
