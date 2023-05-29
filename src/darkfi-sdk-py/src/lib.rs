30446f3343b28e769e00f897386c94c7c7dedac8
freerangedev <>
2023-05-29 04:57:55 +0000

    tidy

--- src/darkfi-sdk-py/src/lib.rs
+++ src/darkfi-sdk-py/src/lib.rs
@@ -1,73 +1,10 @@
 use darkfi_sdk::crypto::{
     pallas,
     pasta_prelude::{Field, PrimeField},
-    poseidon_hash as poseidon_hash_,
 };
 use pyo3::prelude::*;
 use rand::rngs::OsRng;
 
-/// QUESTION: How do export a class that is defined in our dependencies?
-// #[pyclass]
-// struct BaseWrappper {
-//     b: Base
-// }
-
-/// This version deals in lists to avoid the problem converting
-/// Rust structs to Python classes.
-// #[pyfunction]
-// fn poseidon_hash_list(messages: Vec<Vec<u8>>) {
-//     println!("messages: {:?}", messages);
-//     let mut bases = vec![];
-//     for m in messages.iter() {
-//         let m: [u8; 32] = m[0..32].try_into().expect("[DARKFI_SDK_PY] slice with incorrect length");
-//         let base = pallas::Base::from_repr(m).unwrap();
-//         bases.push(base);
-//     }
-//     println!("{:?}", bases);
-
-//     // QUESTION: How to convert bases into [pallas::Base; N]? Is this even possible?
-//     println!("{:?}", poseidon_hash_(bases));
-// }
-
-#[derive(Clone)]
-#[pyclass]
-struct Foo {
-    x: u64,
-}
-
-/// The Foo struct
-#[pymethods]
-impl Foo {
-    #[new]
-    fn new(x: u64) -> Self {
-        Foo { x }
-    }
-    // don't know why self: &Self doesn't work
-    // why can Self be a return type but not &Self?
-    fn bar(&self) -> Self {
-        self.clone()
-    }
-}
-
-/// Binding that comes with the bolierplate.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-/// On how to do submodules: https://pyo3.rs/v0.18.3/module#python-submodules
-/// Binding that comes with the bolierplate.
-/// The #[pymodule] procedural macro takes care of exporting the initialization function of your module to Python.
-#[pymodule]
-fn darkfi_sdk_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_class::<Foo>()?;
-    m.add_class::<PallasBaseWrapper>()?;
-    Ok(())
-}
-
-// ======================================= Actual implementation =========================================
-
-// new type pattern
 #[pyclass]
 struct PallasBaseWrapper(pallas::Base);
 
@@ -115,11 +52,12 @@ impl PallasBaseWrapper {
         Self(self.0.mul(&rhs.0))
     }
 }
-// // associated types: https://doc.rust-lang.org/book/ch19-03-advanced-traits.html#using-the-newtype-pattern-to-implement-external-traits-on-external-types
-// // to get every method inner type has
-// impl Deref for PallasBaseWrapper {
-//     type Target = pallas::Base;
-//     fn deref(&self) -> &pallas::Base { &self.0 }
-// }
 
-
+/// On how to do submodules: https://pyo3.rs/v0.18.3/module#python-submodules
+/// Binding that comes with the bolierplate.
+/// The #[pymodule] procedural macro takes care of exporting the initialization function of your module to Python.
+#[pymodule]
+fn darkfi_sdk_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PallasBaseWrapper>()?;
+    Ok(())
+}
