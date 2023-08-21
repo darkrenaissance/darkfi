5e839ac2371309454e79f6ebae452c13ad5f14d6
parazyd <parazyd@dyne.org>
2023-08-21 11:54:22 +0200

    nodetool: Add license header.

--- script/nodetool.py
+++ script/nodetool.py
@@ -1,4 +1,21 @@
+# This file is part of DarkFi (https://dark.fi)
+#
+# Copyright (C) 2020-2023 Dyne.org foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import asyncio, json, random, sys
+
 
 class JsonRpc:
 
@@ -44,6 +61,7 @@ class JsonRpc:
     #async def dnet_info(self):
     #    return await self._make_request("dnet_info", [])
 
+
 async def main(argv):
     rpc = JsonRpc()
     await rpc.start("localhost", 26660)
@@ -57,5 +75,5 @@ async def main(argv):
     await rpc.dnet_switch(False)
     await rpc.stop()
 
-asyncio.run(main(sys.argv))
 
+asyncio.run(main(sys.argv))
