d030a19be4df3705998dd9de909f0361b61a5f4c
parazyd <parazyd@dyne.org>
2023-08-31 09:53:04 +0200

    rpc: Add missing license headers.

--- src/rpc/util.rs
+++ src/rpc/util.rs
@@ -1,4 +1,23 @@
+/* This file is part of DarkFi (https://dark.fi)
+ *
+ * Copyright (C) 2020-2023 Dyne.org foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::collections::HashMap;
+
 pub use tinyjson::JsonValue::{
     self, Array as JsonArray, Number as JsonNum, Object as JsonObj, String as JsonStr,
 };
@@ -7,6 +26,7 @@ pub use tinyjson::JsonValue::{
 pub fn json_map<const N: usize>(vals: [(&str, JsonValue); N]) -> JsonValue {
     JsonObj(HashMap::from(vals.map(|(k, v)| (k.to_string(), v))))
 }
+
 pub fn json_str(val: &str) -> JsonValue {
     JsonStr(val.to_string())
 }
