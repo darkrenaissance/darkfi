addbaebb4169d118963b62fb494eed6d6b3b4be9
y <y>
2023-09-06 10:14:32 -0400

    Add header to existing scripts

--- script/research/lotterysim/reports/stake_reward.py
+++ script/research/lotterysim/reports/stake_reward.py
@@ -1,3 +1,21 @@
+/* This file is part of DarkFi (https://dark.fi)
+ *
+ * Copyright (C) 2020-2023 Dyne.org foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 # running time ONE_YEAR
 #network airdrop: 2100000000.0, staked token: 2099815635.398858/99.99122073327895% on 1000 nodes
 #acc: 35.66458096862327, reward: 0.005966962921188361850310030975% with instant finality: 0.01673078095726943897690808440%
