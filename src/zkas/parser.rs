2789aa9eee79382980390410341170a08ac59c78
y <y>
2023-09-14 11:52:50 -0400

    add code comment to explain

--- src/zkas/parser.rs
+++ src/zkas/parser.rs
@@ -890,6 +890,11 @@ impl Parser {
                     ))
                 }
 
+                // At this stage of parsing, we should have assigned `stmt` a StatementType that is
+                // not a Noop. If we have failed to do so, we cannot proceed because Nooops must
+                // never be pased to the compiler. This can occur when multiple independent
+                // statements are passed on one line, or if a statement is not terminated by a
+                // semicolon.
                 if stmt.typ == StatementType::Noop {
                     return Err(self.error.abort(
                         "Statement is a NOOP; not allowed. (Did you miss a semicolon?)",
