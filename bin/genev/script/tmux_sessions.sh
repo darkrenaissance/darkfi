0808c13a7c0a6362df7544ad827d939ae4d70426
Dastan-glitch <Dastan.ManDan@gmail.com>
2023-11-11 05:03:11 +0300

    bin/genev: fix test script

--- bin/genev/script/tmux_sessions.sh
+++ bin/genev/script/tmux_sessions.sh
@@ -3,7 +3,7 @@
 set -e
 
 tmux new-session -s "genevd" -n "genevd" -d
-tmux send-keys "../../../genevd --localnet --config genevd_seed.toml" Enter && sleep 1
+tmux send-keys "../../../genevd --localnet --config genevd_seed.toml --skip-dag-sync" Enter && sleep 1
 tmux split-window -h
 tmux send-keys "../../../genevd --localnet --config genevd_a.toml" Enter
 tmux split-window -h
@@ -17,7 +17,7 @@ tmux send-keys "../../../genevd --localnet --config genevd_d.toml" Enter
 
 
 tmux new-window -t "genevd:1" -n "genev"
-sleep 1
+sleep 5
 tmux send-keys "../../../genev -e tcp://127.0.0.1:28870 add alolymous \"pay bills\" \"gonna pay some bills in the morning\" " Enter
 tmux split-window -v
 sleep 1
