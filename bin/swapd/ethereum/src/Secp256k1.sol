a8a739808fed6bf6ca6a70765523ae09b7f16aa7
elizabeth <elizabethjbinks@gmail.com>
2023-12-08 19:04:03 -0500

    init contracts

---
+++ bin/swapd/ethereum/src/Secp256k1.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: LGPLv3
+// Implemention based on Vitalik's idea:
+// https://ethresear.ch/t/you-can-kinda-abuse-ecrecover-to-do-ecmul-in-secp256k1-today
+
+pragma solidity ^0.8.19;
+
+contract Secp256k1 {
+    // solhint-disable-next-line
+    uint256 private constant gx =
+        0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
+    // solhint-disable-next-line
+    uint256 private constant m = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+
+    // mulVerify returns true if `Q = s * G` on the secp256k1 curve
+    // qKeccak is defined as uint256(keccak256(abi.encodePacked(qx, qy))
+    function mulVerify(uint256 scalar, uint256 qKeccak) public pure returns (bool) {
+        address qRes = ecrecover(0, 27, bytes32(gx), bytes32(mulmod(scalar, gx, m)));
+        return uint160(qKeccak) == uint160(qRes);
+    }
+}
--- bin/swapd/ethereum/src/SwapCreator.sol
+++ bin/swapd/ethereum/src/SwapCreator.sol
@@ -5,23 +5,6 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Secp256k1} from "./Secp256k1.sol";
 
-// Implemention based on Vitalik's idea:
-// https://ethresear.ch/t/you-can-kinda-abuse-ecrecover-to-do-ecmul-in-secp256k1-today
-contract Secp256k1 {
-    // solhint-disable-next-line
-    uint256 private constant gx =
-        0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
-    // solhint-disable-next-line
-    uint256 private constant m = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
-
-    // mulVerify returns true if `Q = s * G` on the secp256k1 curve
-    // qKeccak is defined as uint256(keccak256(abi.encodePacked(qx, qy))
-    function mulVerify(uint256 scalar, uint256 qKeccak) public pure returns (bool) {
-        address qRes = ecrecover(0, 27, bytes32(gx), bytes32(mulmod(scalar, gx, m)));
-        return uint160(qKeccak) == uint160(qRes);
-    }
-}
-
 // SwapCreator facilitates swapping between Alice, a party that has an EVM
 // native currency or a token (ERC-20 or compatible API) that she wants to
 // exchange cross-chain for a different currency, and Bob, a party that has the
