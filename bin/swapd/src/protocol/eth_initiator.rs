787676348cde84d5897976f308da5d631056d4db
elizabeth <elizabethjbinks@gmail.com>
2023-12-08 19:19:11 -0500

    setup contracts w forge

--- bin/swapd/src/protocol/eth_initiator.rs
+++ bin/swapd/src/protocol/eth_initiator.rs
@@ -1,1 +1,23 @@
-use crate::protocol::traits::{Initiator};
+use crate::protocol::traits::Initiator;
+
+pub(crate) struct EthInitiator {
+    
+}
+
+impl Initiator for EthInitiator {
+    fn handle_counterparty_keys_received(&self) {
+        // ...
+    }
+
+    fn handle_counterparty_funds_locked(&self) {
+        // ...
+    }
+
+    fn handle_counterparty_funds_claimed(&self) {
+        // ...
+    }
+
+    fn handle_should_refund(&self) {
+        // ...
+    }
+}
