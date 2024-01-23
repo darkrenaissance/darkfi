badd907efc2dfea38a5358720d04abee5bd870d9
parazyd <parazyd@dyne.org>
2024-01-23 18:01:04 +0100

    hosts: Wrap module rustdoc

--- src/net/hosts/mod.rs
+++ src/net/hosts/mod.rs
@@ -16,24 +16,30 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/// Periodically probe entries in the greylist. Randomly selects a greylist entry and tries to
-/// establish a local connection to it using the method probe_node(), which creates a channel and
-/// does a version exchange using perform_handshake_protocols().
+/// Periodically probes entries in the greylist.
 ///
-/// If successful, the entry is removed from the greylist and added to the whitelist with an
-/// updated last_seen timestamp. If non-successful, the entry is removed from the greylist.
+/// Randomly selects a greylist entry and tries to
+/// establish a local connection to it using the method probe_node(), which creates
+/// a channel and does a version exchange using `perform_handshake_protocols()`.
 ///
-/// The method probe_node() is also used by ProtocolSeed and ProtocolAddress. We try to establish
-/// local connections to our own external addresses using probe_node() to ensure the address is valid
-/// before propagating in ProtocolSeed and ProtocolAddress.
+/// If successful, the entry is removed from the greylist and added to the whitelist
+/// with an updated last_seen timestamp. If non-successful, the entry is removed from
+/// the greylist.
+///
+/// The method `probe_node()` is also used by `ProtocolSeed` and `ProtocolAddress`.
+/// We try to establish local connections to our own external addresses using
+/// `probe_node()` to ensure the address is valid before propagating in `ProtocolSeed`
+/// and `ProtocolAddress`.
 pub mod refinery;
 
-/// The main interface for interacting with the hostlist, which is stored in three sections: white,
-/// grey and anchorlists. The whitelist contains hosts that have been seen recently, the anchorlist
-/// contains hosts that we have been able to establish a connection to, and the greylist is an
-/// intermediary host list of recently received hosts that is periodically refreshed using the
-/// greylist refinery.
+/// The main interface for interacting with the hostlist.
 ///
-/// Store contains various methods for reading from, quering and writing to the hostlists. It is
-/// also responsible for filtering addresses and ensuring channel transport validity.
+/// The hostlist is stored in three sections: white, grey, and anchorlists.
+/// The _whitelist_ contains hosts that have been seen recently.
+/// The _anchorlist_ contains hosts that we have been able to establish a connection to.
+/// The _greylist_ is an intermediary host list of recently received hosts that is
+/// periodically refreshed using the greylist refinery.
+///
+/// `store` contains various methods for reading from, quering and writing to the hostlists.
+/// It is also responsible for filtering addresses and ensuring channel transport validity.
 pub(super) mod store;
