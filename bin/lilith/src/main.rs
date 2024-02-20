c25df52c64559b31f0c9c52b17064e7dc8eac197
nighthawk24 <nighthawk24@users.noreply.github.com>
2024-02-20 15:57:02 +0100

    fud | lilith | research/dhtd/ | geode | tests : Refactor to return match statements

--- bin/lilith/src/main.rs
+++ bin/lilith/src/main.rs
@@ -222,10 +222,10 @@ impl Lilith {
 #[async_trait]
 impl RequestHandler for Lilith {
     async fn handle_request(&self, req: JsonRequest) -> JsonResult {
-        match req.method.as_str() {
-            "ping" => return self.pong(req.id, req.params).await,
-            "spawns" => return self.spawns(req.id, req.params).await,
-            _ => return JsonError::new(ErrorCode::MethodNotFound, None, req.id).into(),
+        return match req.method.as_str() {
+            "ping" => self.pong(req.id, req.params).await,
+            "spawns" => self.spawns(req.id, req.params).await,
+            _ => JsonError::new(ErrorCode::MethodNotFound, None, req.id).into(),
         }
     }
 
--- tests/jsonrpc.rs
+++ tests/jsonrpc.rs
@@ -59,10 +59,10 @@ impl RequestHandler for RpcSrv {
     async fn handle_request(&self, req: JsonRequest) -> JsonResult {
         assert!(req.params.is_array());
 
-        match req.method.as_str() {
-            "ping" => return self.pong(req.id, req.params).await,
-            "kill" => return self.kill(req.id, req.params).await,
-            _ => return JsonError::new(ErrorCode::MethodNotFound, None, req.id).into(),
+        return match req.method.as_str() {
+            "ping" => self.pong(req.id, req.params).await,
+            "kill" => self.kill(req.id, req.params).await,
+            _ => JsonError::new(ErrorCode::MethodNotFound, None, req.id).into(),
         }
     }
 
