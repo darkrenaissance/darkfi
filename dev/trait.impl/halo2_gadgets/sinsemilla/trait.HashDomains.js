3de40f549190ca057ecca38b31be45a1b7cdc83a
bookworm <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 13:32:48 +0000

    Generate mdbook

---
+++ dev/trait.impl/halo2_gadgets/sinsemilla/trait.HashDomains.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["darkfi_sdk",[["impl HashDomains&lt;EpAffine&gt; for <a class=\"enum\" href=\"darkfi_sdk/crypto/constants/sinsemilla/enum.OrchardHashDomains.html\" title=\"enum darkfi_sdk::crypto::constants::sinsemilla::OrchardHashDomains\">OrchardHashDomains</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[252]}
