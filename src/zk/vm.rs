ad2866299f96ff335234150b485a8085b01cb4d3
skoupidi <skoupidi@systemli.org>
2024-12-24 12:55:02 +0200

    zk/vm: increase poseidon hash elements limit to 24

--- src/zk/vm.rs
+++ src/zk/vm.rs
@@ -971,7 +971,7 @@ impl Circuit<pallas::Base> for ZkCircuit {
                         };
                     }
 
-                    vla!(args, a, b, c, 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16);
+                    vla!(args, a, b, c, 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24);
                 }
 
                 Opcode::MerkleRoot => {
