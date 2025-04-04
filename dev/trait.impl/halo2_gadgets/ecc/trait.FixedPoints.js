fb920ad9eb01394cd3111a60436be7d34888ea5e
bookworm <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 17:14:12 +0000

    Generate mdbook

---
+++ dev/trait.impl/halo2_gadgets/ecc/trait.FixedPoints.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["darkfi_sdk",[["impl FixedPoints&lt;EpAffine&gt; for <a class=\"enum\" href=\"darkfi_sdk/crypto/constants/fixed_bases/enum.OrchardFixedBases.html\" title=\"enum darkfi_sdk::crypto::constants::fixed_bases::OrchardFixedBases\">OrchardFixedBases</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[251]}
---
+++ dev/trait.impl/rustls/verify/trait.ServerCertVerifier.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["darkfi",[["impl ServerCertVerifier for <a class=\"struct\" href=\"darkfi/net/transport/tls/struct.ServerCertificateVerifier.html\" title=\"struct darkfi::net::transport::tls::ServerCertificateVerifier\">ServerCertificateVerifier</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[238]}
