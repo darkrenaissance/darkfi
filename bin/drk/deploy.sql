37d89d5da0d48f2077542a767392b5e48c15bfb3
skoupidi <skoupidi@systemli.org>
2025-06-07 13:27:11 +0300

    drk: add freeze block height reference to deployoOor

--- bin/drk/deploy.sql
+++ bin/drk/deploy.sql
@@ -4,5 +4,6 @@
 CREATE TABLE IF NOT EXISTS EJs7oEjKkvCeEVCmpRsd6fEoTGCFJ7WKUBfmAjwaegN_deploy_auth (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	deploy_authority BLOB UNIQUE NOT NULL,
-	is_frozen INTEGER NOT NULL
+	is_frozen INTEGER NOT NULL,
+	freeze_height INTEGER
 );
