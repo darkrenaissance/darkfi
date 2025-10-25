21a11ab32aa697169a702ebee111ed3499618547
bookworm <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-25 16:56:02 +0000

    Generate mdbook

---
+++ dev/darkfi/net/session/direct_session/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ChannelBuilder","DirectSession","PeerDiscovery"],"type":["DirectSessionPtr"]};
---
+++ dev/darkfi/net/session/refine_session/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["GreylistRefinery","RefineSession"],"type":["RefineSessionPtr"]};
